7ef6cd0acd593257633bea45bbec6ba84fc49638
amritacerti <amrita.certi@gmail.com>
2025-03-11 21:22:37 +0530

    first commit

---
+++ demo/src/test/java/com/example/azureapp/controller/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azureapp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
